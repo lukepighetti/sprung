695408d7415755803954007756564c615fe4b501
lukepighetti <lukepighetti@gmail.com>
2020-11-24 10:45:11 -0500

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,149 +1,139 @@
 import 'package:flutter/material.dart';
-
 import 'package:sprung/sprung.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SprungDemoApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SprungDemoApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sprung Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.pink,
-      ),
-      home: MyHomePage(title: 'Sprung Demo'),
-    );
-  }
+  _SprungDemoAppState createState() => _SprungDemoAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _SprungDemoAppState extends State<SprungDemoApp> {
+  static final title = 'Sprung Demo';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  /// The state map used to handle the animation toggle state for each [_AnimatedTrack].
+  final toggleMap = {
+    'under': false,
+    'critically': false,
+    'over': false,
+  };
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  /// The String/Curve map to build out our example [_AnimatedTrack]s.
+  final sprungMap = {
+    'under': Sprung.underDamped,
+    'critically': Sprung.criticallyDamped,
+    'over': Sprung.overDamped,
+  };
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _toggleOffset method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.all(24.0),
-
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SprungBox(
-              title: "under",
-              curve: Sprung.underDamped,
-            ),
-            SprungBox(
-              title: "critically",
-              curve: Sprung.criticallyDamped,
-            ),
-            SprungBox(
-              title: "over",
-              curve: Sprung.overDamped,
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class SprungBox extends StatefulWidget {
-  final String title;
-  final Curve curve;
-  final Duration duration;
-
-  SprungBox({
-    @required this.title,
-    @required this.curve,
-    duration,
-  }) : this.duration = duration ?? Duration(milliseconds: 700);
-
-  @override
-  _SprungBoxState createState() => _SprungBoxState();
-}
-
-class _SprungBoxState extends State<SprungBox> {
-  bool _isOffset = false;
-
-  void _toggleOffset() {
+  /// Toggle a single track based on a key from [sprungMap] and [toggleMap].
+  void handleToggle(String e) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      this._isOffset = !this._isOffset;
+      toggleMap[e] = !toggleMap[e];
+    });
+  }
+
+  /// Toggle all tracks in [toggleMap].
+  void handleToggleAll() {
+    setState(() {
+      toggleMap.forEach((key, _) {
+        handleToggle(key);
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final height = constraints.maxHeight;
-        final text = widget.title;
-        final top = this._isOffset ? height - 200.0 : 100.0;
-
-        final width = MediaQuery.of(context).size.width / 3 - 30;
-
-        return GestureDetector(
-          onTap: _toggleOffset,
-          child: AnimatedContainer(
-            duration: this.widget.duration,
-            curve: widget.curve,
-            margin: EdgeInsets.only(
-              top: top,
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.pink),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        /// App bar
+        appBar: AppBar(
+          title: Text(title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.swap_vert),
+              onPressed: handleToggleAll,
             ),
-            height: width,
-            width: width,
-            color: Colors.pinkAccent,
-            child: Center(
-              child: Text(
-                text,
-                style: TextStyle(color: Colors.white),
+          ],
+        ),
+        body: Container(
+          alignment: Alignment.topCenter,
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            children: <Widget>[
+              for (var e in sprungMap.entries)
+
+                /// Track
+                Expanded(
+                  child: _AnimatedTrack(
+                    title: e.key,
+                    curve: e.value,
+                    toggle: toggleMap[e.key],
+                    onTap: () => handleToggle(e.key),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AnimatedTrack extends StatelessWidget {
+  /// A vertical track with animated alignment for a square tappable box with text.
+  ///
+  /// At the top of the track when [toggle] is true.
+  _AnimatedTrack({
+    Key key,
+    @required this.title,
+    @required this.curve,
+    this.toggle = false,
+    this.onTap,
+  }) : super(key: key);
+
+  /// The title to display on this animated track.
+  final String title;
+
+  /// The curve to animate with.
+  final Curve curve;
+
+  /// The callback to fire when tapping the button in this track.
+  final VoidCallback onTap;
+
+  /// If this track is toggled or not.
+  final bool toggle;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return TweenAnimationBuilder(
+      tween: Tween(end: toggle ? 1.0 : 0.0),
+      curve: curve,
+      duration: Duration(milliseconds: 750),
+      builder: (context, t, child) {
+        return Container(
+          margin: EdgeInsets.all(8),
+          alignment: Alignment.lerp(
+            Alignment.topCenter,
+            Alignment.bottomCenter,
+            t,
+          ),
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: Material(
+              color: theme.primaryColor,
+              child: InkWell(
+                onTap: onTap,
+                child: Center(
+                  child: Text(
+                    title,
+                    style: theme.primaryTextTheme.subtitle2,
+                  ),
+                ),
               ),
             ),
           ),
--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,17 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_test/flutter_test.dart';
 
 import '../lib/main.dart';
 
 void main() {
   testWidgets('Demo runs', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SprungDemoApp());
     expect(find.text('Sprung Demo'), findsOneWidget);
   });
 }
